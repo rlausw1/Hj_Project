5991ed555321e9b05465f5cb5db194d2172aa23f
김현지 <guswlhjk@naver.com>
2021-09-13 17:16:02 +0900

    BaseActivity 세팅

--- app/src/main/java/com/nepplus/myproject/BaseActivity.kt
+++ app/src/main/java/com/nepplus/myproject/BaseActivity.kt
@@ -1,4 +1,17 @@
 package com.nepplus.myproject
 
-class BaseActivity {
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity()  {
+
+    lateinit var mContext: Context
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mContext = this
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
 }
