814424f702d96278ef6198699eb76b3e6ceca0a8
김현지 <guswlhjk@naver.com>
2021-09-14 04:44:39 +0900

    서버가 알려준 약속 목록을 파싱  - 변수 : List<데이터클래스> 로 파싱.  - 응답.data.목록 => 실제 코틀린에 더 맞게 활용 가능. (ex. addall 재료)

--- app/src/main/java/com/nepplus/myproject/datas/DataResponse.kt
+++ app/src/main/java/com/nepplus/myproject/datas/DataResponse.kt
@@ -3,8 +3,13 @@ package com.nepplus.myproject.datas
 import android.service.autofill.UserData
 
 class DataResponse(
+//    로그인 성공시 파싱용 변수
+
     var token: String,
     var user: com.nepplus.myproject.datas.UserData
+//    이 밑으로는 약속 목록파싱용 변수
+    var appointments : List<AppointmentData>
+
 ) {
 
 
