40377a384078cc08ec8376cf82259bc08987460c
김현지 <guswlhjk@naver.com>
2021-09-13 17:57:37 +0900

    페북 로그인 버튼 활용 - 로그인 처리

--- app/src/main/java/com/nepplus/myproject/LoginActivity.kt
+++ app/src/main/java/com/nepplus/myproject/LoginActivity.kt
@@ -1,14 +1,30 @@
 package com.nepplus.myproject
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import com.facebook.CallbackManager
 import com.nepplus.myproject.databinding.ActivityLoginBinding
 import java.security.MessageDigest
+import com.facebook.FacebookException
+
+import com.facebook.login.LoginResult
+
+import com.facebook.FacebookCallback
+
+import android.view.View
+
+import com.facebook.login.widget.LoginButton
+
+
+
 
 class LoginActivity : BaseActivity() {
+    
+    lateinit var callbackManager : CallbackManager
 
     lateinit var binding: ActivityLoginBinding
 
@@ -27,6 +43,32 @@ class LoginActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        callbackManager = CallbackManager.Factory.create();
+        binding.loginButton.setReadPermissions("email")
+
+
+
+
+        // Callback registration
+        binding.loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                // App code
+
+                Log.d("확인용", loginResult.toString())
+            }
+
+            override fun onCancel() {
+                // App code
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+
+
+            }
+        })
+
+
     }
 
     override fun setValues() {
@@ -40,5 +82,10 @@ class LoginActivity : BaseActivity() {
             md.update(signature.toByteArray())
             Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
