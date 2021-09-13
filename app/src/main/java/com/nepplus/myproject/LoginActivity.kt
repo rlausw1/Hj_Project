02e921625c3cad2f76281002004606360315654b
김현지 <guswlhjk@naver.com>
2021-09-13 17:45:19 +0900

    keyHash 추출

---
+++ app/src/main/java/com/nepplus/myproject/LoginActivity.kt
@@ -0,0 +1,44 @@
+package com.nepplus.myproject
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.nepplus.myproject.databinding.ActivityLoginBinding
+import java.security.MessageDigest
+
+class LoginActivity : BaseActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        setupEvents()
+        setValues()
+
+
+
+
+
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+        val info = packageManager.getPackageInfo(
+        "com.nepplus.myproject",
+        PackageManager.GET_SIGNATURES
+        )
+        for (signature in info.signatures) {
+            val md: MessageDigest = MessageDigest.getInstance("SHA")
+            md.update(signature.toByteArray())
+            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+        }
+    }
+}
