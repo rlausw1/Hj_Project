80da41fd6bc53bc4904419c7fddefef3f86dc5b0
김현지 <guswlhjk@naver.com>
2021-09-13 18:04:03 +0900

    페북 로그인 버튼 추가 이벤트 부여  - LoginManager 클래스 활용

--- app/src/main/java/com/nepplus/myproject/LoginActivity.kt
+++ app/src/main/java/com/nepplus/myproject/LoginActivity.kt
@@ -16,10 +16,11 @@ import com.facebook.login.LoginResult
 import com.facebook.FacebookCallback
 
 import android.view.View
+import com.facebook.AccessToken
+import com.facebook.login.LoginManager
 
 import com.facebook.login.widget.LoginButton
-
-
+import java.util.*
 
 
 class LoginActivity : BaseActivity() {
@@ -45,6 +46,10 @@ class LoginActivity : BaseActivity() {
 
         callbackManager = CallbackManager.Factory.create();
         binding.loginButton.setReadPermissions("email")
+        binding.facebookLoginBtn.setOnClickListener {
+//             우리가 붙인 버튼에 기능 활용
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"))
+        }
 
 
 
@@ -55,6 +60,9 @@ class LoginActivity : BaseActivity() {
                 // App code
 
                 Log.d("확인용", loginResult.toString())
+
+                val accessToken = AccessToken.getCurrentAccessToken()
+                Log.d("페북토큰", accessToken.toString())
             }
 
             override fun onCancel() {
