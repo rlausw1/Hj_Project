43efd6c0ff05c845e343d30a4da32a0db1f1320e
김현지 <guswlhjk@naver.com>
2021-09-14 00:51:04 +0900

    소셜로그인 성공시에도 토큰 저장  - 우리 API서버가 발급해준 토큰

--- app/src/main/java/com/nepplus/myproject/LoginActivity.kt
+++ app/src/main/java/com/nepplus/myproject/LoginActivity.kt
@@ -29,8 +29,8 @@ import java.util.*
 
 
 class LoginActivity : BaseActivity() {
-    
-    lateinit var callbackManager : CallbackManager
+
+    lateinit var callbackManager: CallbackManager
 
     lateinit var binding: ActivityLoginBinding
 
@@ -39,10 +39,6 @@ class LoginActivity : BaseActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
-
-
-
-
 
 
     }
@@ -63,11 +59,9 @@ class LoginActivity : BaseActivity() {
 
                         Log.d("토큰", basicResponse.data.token)
                         ContextUtil.setToken(mContext, basicResponse.data.token)
-                        
 
 
-                    }
-                    else {
+                    } else {
                         val errorBodyStr = response.errorBody()!!.string()
                         val jsonObj = JSONObject(errorBodyStr)
                         Log.d("응답본문", jsonObj.toString())
@@ -94,20 +88,39 @@ class LoginActivity : BaseActivity() {
             UserApiClient.instance.loginWithKakaoAccount(mContext) { token, error ->
                 if (error != null) {
                     Log.e("카카오로그인", "로그인 실패", error)
-                }
-                else if (token != null) {
+                } else if (token != null) {
                     Log.i("카카오로그인", "로그인 성공 ${token.accessToken}")
 
                     UserApiClient.instance.me { user, error ->
                         if (error != null) {
                             Log.e("카카오로그인", "사용자 정보 요청 실패", error)
-                        }
-                        else if (user != null) {
-                            Log.i("카카오로그인", "사용자 정보 요청 성공" +
-                                    "\n회원번호: ${user.id}" +
-                                    "\n이메일: ${user.kakaoAccount?.email}" +
-                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
-                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+                        } else if (user != null) {
+                            Log.i(
+                                "카카오로그인", "사용자 정보 요청 성공" +
+                                        "\n회원번호: ${user.id}" +
+                                        "\n이메일: ${user.kakaoAccount?.email}" +
+                                        "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                                        "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+                            )
+
+//                           소셜로그인 api에 "kakao"로 id / 닉네임 전송
+                            apiService.postRequestSocialLogin("kakao",
+                                user.id.toString(),
+                                user.kakaoAccount?.profile?.nickname!!).enqueue(object  : Callback<BasicResponse> {
+                                override fun onResponse(
+                                    call: Call<BasicResponse>,
+                                    response: Response<BasicResponse>
+                                ) {
+                                    val basicResponse = response.body()!!
+                                    ContextUtil.setToken(mContext, basicResponse.data.token)
+
+                                }
+
+                                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                                }
+                                })
+
                         }
                     }
 
@@ -123,66 +136,82 @@ class LoginActivity : BaseActivity() {
 //             우리가 붙인 버튼에 기능 활용
 //            커스터 버튼에 로그인 하고 돌아온 callback을 따로 설정
 
-            LoginManager.getInstance().registerCallback(callbackManager, object  : FacebookCallback<LoginResult>{
-                override fun onSuccess(result: LoginResult?) {
-                    Log.d("로그인 성공", "우리가 만든 버튼으로 성공")
+            LoginManager.getInstance()
+                .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(result: LoginResult?) {
+                        Log.d("로그인 성공", "우리가 만든 버튼으로 성공")
 //                    페이스북에 접근할수 있는 토큰이 생겼다
 //                    로그인한 사람 정보를 받아오는데 활용
-                    val graphRequest = GraphRequest.newMeRequest(result?.accessToken, object : GraphRequest.GraphJSONObjectCallback {
-                        override fun onCompleted(jsonObj : JSONObject?, response: GraphResponse?) {
+                        val graphRequest = GraphRequest.newMeRequest(
+                            result?.accessToken,
+                            object : GraphRequest.GraphJSONObjectCallback {
+                                override fun onCompleted(
+                                    jsonObj: JSONObject?,
+                                    response: GraphResponse?
+                                ) {
 
-                            Log.d("내정보내용", jsonObj.toString())
-                            val name = jsonObj!!.getString("name")
-                            val id = jsonObj.getString("id")
+                                    Log.d("내정보내용", jsonObj.toString())
+                                    val name = jsonObj!!.getString("name")
+                                    val id = jsonObj.getString("id")
 
 //                           가입한 회원 이름 => 우리 서버에 사용자 이름으로 (닉네임) 저장
-                            Log.d("이름", name)
+                                    Log.d("이름", name)
 //                            페북에서 사용자를 구별하는 고유번호 => 우리 서버에 같이 저장. 회원가입 또는 로그인 근거자료로 활용용
-                           Log.d("id값", id)
+                                    Log.d("id값", id)
 
 
-                            apiService.postRequestSocialLogin("facebook", id, name).enqueue(object  : Callback<BasicResponse> {
-                                override fun onResponse(
-                                    call: Call<BasicResponse>,
-                                    response: Response<BasicResponse>
-                                ) {
-                                    val basicResponse = response.body()!!
-                                    Toast.makeText(mContext, basicResponse.message, Toast.LENGTH_SHORT).show()
-                                    Log.d("API 서버가 준 토큰값", basicResponse.data.token)
+                                    apiService.postRequestSocialLogin("facebook", id, name)
+                                        .enqueue(object : Callback<BasicResponse> {
+                                            override fun onResponse(
+                                                call: Call<BasicResponse>,
+                                                response: Response<BasicResponse>
+                                            ) {
+                                                val basicResponse = response.body()!!
+                                                Toast.makeText(
+                                                    mContext,
+                                                    basicResponse.message,
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                                Log.d("API 서버가 준 토큰값", basicResponse.data.token)
 
 //                                    ContextUtil 등으로 SharedPreferences로 토큰값 저장.
-
+                                                ContextUtil.setToken(
+                                                    mContext,
+                                                    basicResponse.data.token
+                                                )
 //                                    메인화면으로 이동.
 
-                                }
+                                            }
 
-                                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
-                                }
+                                            override fun onFailure(
+                                                call: Call<BasicResponse>,
+                                                t: Throwable
+                                            ) {
+                                            }
 
+
+                                        })
+
+                                }
 
                             })
-
-                        }
-
-                    })
 //                    위에서 정리한 내용을 들고, 내 정보를 실제로 요청
-                    graphRequest.executeAsync()
+                        graphRequest.executeAsync()
 
-                }
+                    }
 
-                override fun onCancel() {
-                }
+                    override fun onCancel() {
+                    }
 
-                override fun onError(error: FacebookException?) {
-                }
+                    override fun onError(error: FacebookException?) {
+                    }
 
 
-            })
+                })
 
-            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"))
+            LoginManager.getInstance()
+                .logInWithReadPermissions(this, Arrays.asList("public_profile"))
         }
-
-
 
 
         // Callback registration
@@ -213,8 +242,8 @@ class LoginActivity : BaseActivity() {
     override fun setValues() {
 
         val info = packageManager.getPackageInfo(
-        "com.nepplus.myproject",
-        PackageManager.GET_SIGNATURES
+            "com.nepplus.myproject",
+            PackageManager.GET_SIGNATURES
         )
         for (signature in info.signatures) {
             val md: MessageDigest = MessageDigest.getInstance("SHA")
